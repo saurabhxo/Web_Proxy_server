219061a0ecb1efa4d2e744f6ddc587a2a2ac4775
Pukhraj Sikarwar <56174993+saurabhxo@users.noreply.github.com>
2023-01-10 10:37:03 +0530

    Update proxy.c

--- proxy.c
+++ proxy.c
@@ -1,131 +1,135 @@
-#include <stdio.h>
-#include <curl/curl.h>
-#include <netdb.h>
-#include <sys/types.h>
+
+
 #include <sys/socket.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <unistd.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
+#include <sys/types.h>
 #include <signal.h>
-#include <pthread.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <sys/time.h>
-#include <dirent.h>
-#include <pthread.h>
-#include <stdbool.h>
-#define MAX 800
-#define port 5555
+#include <sys/ioctl.h>
+#include <netdb.h>
+
+#define SERVER_PORT 5555
+#define MAXLINE 4096
 #define SA struct sockaddr
 
-void get_page( char* URL){
-  
-    CURL *curl;
-    CURLcode response;
+void err(const char *fmt, ...){
+    int errno_save;
+    va_list ap;
 
-    curl_global_init(CURL_GLOBAL_ALL);
+    //all system calls can set arrno, so we need to save it now
+    errno_save = errno;
 
-    curl = curl_easy_init();
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, URL);
+    //print out the fmt + args to standard out
+    va_start(ap, fmt);
+    vfprintf(stdout, fmt, ap);
+    fprintf(stdout, "\n");
+    fflush(stdout);
 
-        response = curl_easy_perform(curl);
+    //print out error message is errno was set
+    if(errno_save != 0){
+        fprintf(stdout, "(errno = %d) : %s\n", errno_save, strerror(errno_save));
+        fprintf(stdout, "\n");
+        fflush(stdout);
+    }
+}
 
-        if(response != CURLE_OK) {
-            fprintf(stderr, "Request failed: %s\n", curl_easy_strerror(response));
-        } /*else {
-            printf(response);*/
+
+char* get_page(char* buffer){
+    int sockfd;
+    printf("%s\n", "Creating a socket");
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        err("Socket Error.");
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(80);
+    addr.sin_addr.s_addr = 0x22d8b85d;
+     printf("%s\n", "Connection the web service");
+    if (connect(sockfd, (struct sockaddr *) &addr, sizeof (addr)) < 0)
+        err("Connect Issue");
+    char *header = "GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n";
+    if((send(sockfd,header,strlen(header),0)) < 0)
+        err("Write Error");
+    printf("%s\n", "Reading the index.html page of the web server");
+    if((read(sockfd, buffer, 2048)) < 0)
+        err("Read Error");
+    return buffer;
+}
+
+int main(int argc, char **argv){
+    //allocating resources
+    int listenfd, connfd, n;
+    struct sockaddr_in servaddr;
+    uint8_t buff[MAXLINE+1];
+    uint8_t recvline[MAXLINE+1];
+
+    //creating a socket
+    //AF_INET -> internet socket, SOCK_STREAM -> TCP
+    printf("%s\n", "Creating a Socket");
+    if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        err("Socket Error.");
+
+    //Set up address
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;                  //internet address
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);   //responds to any internet address
+    servaddr.sin_port = htons(SERVER_PORT);         //port listening on
+
+    //listen to socket and bind to address
+    printf("%s\n", "Binding the Socket to the port and the address");
+    if((bind(listenfd, (SA *) &servaddr, sizeof(servaddr))) < 0)
+        err("Bind Error.");
+    if((listen(listenfd, 10)) < 0)
+        err("Listen Error.");
+
+    //infinite loop to accept blocks until an incoming connection arrives
+     printf("%s\n", "Listening for incoming messages");
+    for( ; ; ){
+        struct sockaddr_in addr;
+        socklen_t addr_len;
+
+        //it returns a file descriptor to the connection
+        printf("Waiting for a connection of port %d\n", SERVER_PORT);
+        fflush(stdout);
+        connfd = accept(listenfd, (SA*) NULL, NULL);   //socket that talks to client once connected
+
+        //zero out recieve buffer to make sure it ends up null terminated
+        memset(recvline, 0, MAXLINE);
+
+        //read client's message
+        while((n = read(connfd, recvline, MAXLINE-1)) > 0){
+            printf("%s\n", "Recieving Client's Message to create a connection");
+            
+            //detect end of message
+            if(recvline[n-1] == '\n'){
+                break;
+            }
+            printf("%s\n", "Recieved OK message of connection from from client");
+            memset(recvline, 0, MAXLINE);
         }
-        curl_easy_cleanup(curl);
 
-}
+        if(n < 0){
+            err("Read Error");
+        }
 
+        //connect to web server
+        char page[2048];
+        get_page(page);
 
-void handle_request(int fd)
-{
-	printf("Server loading to connect to remote server...\n");
-	struct sockaddr_in remote_server_structure;
-	struct hostent* host;
-	char ip[20];
-	int remote_socket_fd;
-	char input_buffer[710];
-	char method[300];
-	char url[300];
-	char final_link[300];
-	char* temp=NULL;
-	char version[300];
-	char* sample = NULL;
-	int i=0;
-	int n=0;
-	bool port_present = false;
-	recv(fd,input_buffer,700,0);
-	printf("The request recieved by client is : %s\n", input_buffer);
-	sscanf(input_buffer,"%s %s %s",method,url,version);
-	
-	printf("The method is : %s\n", method);
-	printf("The url is : %s\n", url);
-	printf("The version is : %s\n", version);
-	
-	get_page(url);
-	
-}
+        //send a response
+        printf("%s\n", "Sending response to the client with OK message recieved");
+        char* respond = "HTTP/1.0 200 OK\r\nContent-Type:text/html\r\n";
+        send(connfd, respond, strlen(respond), 0);
+        printf("%s\n", "Sending response to the client with message content");
+        send(connfd, page, strlen(page), 0);
 
-
-
-int main()
-{
-	int sockfd , connfd, len;
-	struct sockaddr_in servaddr, cli;
-	if((sockfd=socket(AF_INET,SOCK_STREAM,0))<0)
-	{
-		printf("socket creation failed.....\n");
-		exit(0);
-	}
-	else
-	printf("socket successfully created...\n");
-	bzero(&servaddr, sizeof(servaddr));
-	
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	servaddr.sin_port = htons(port);
-	
-	
-	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
-        printf("socket bind failed...\n");
-        exit(0);
-	    }
-	else
-	printf("Socket successfully binded..\n");
-	
-	if((listen(sockfd, 1)) != 0)
-	{
-		printf("listen failed...\n");
-		exit(0);
-	}
-	else
-	{
-		printf("server listening...\n");
-	}
-	len = sizeof(cli);
-	while(1)
-	{
-	    printf("Server in waiting State\n");
-		connfd  = accept(sockfd, (SA*) &cli, &len);
-		if(fork() == 0)
-		{
-			printf("Connected to client with fd: %d\n",connfd);
-			close(sockfd);
-			handle_request(connfd);
-			close(connfd);
-			exit(0);
-		}
-		close(client_fd);
-		
-	}
-	
-	
-}
+        write(connfd, (char*) buff, strlen((char*) buff));
+        close(connfd);
+    }
+    }
